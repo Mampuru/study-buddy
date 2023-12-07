bc984aa27863b7c1fec64a1f3b101088883ba288
Mampuru <mampuruntwampe@gmail.com>
2023-12-07 23:46:19 +0200

    Addition of flash card hive adapter class

---
+++ lib/hive_adapters/flash_card_adapter.dart
@@ -0,0 +1,23 @@
+class FlashcardAdapter extends TypeAdapter<Flashcard> {
+  @override
+  final int typeId = 0; // Assign a unique ID
+
+  @override
+  Flashcard read(BinaryReader reader) {
+    // Implement how to read data from Hive
+    return Flashcard(
+      question: reader.readString(),
+      answer: reader.readString(),
+      category: reader.readString(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Flashcard obj) {
+    // Implement how to write data to Hive
+    writer.writeString(obj.question);
+    writer.writeString(obj.answer);
+    writer.writeString(obj.category);
+  }
+}
+
