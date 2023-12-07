6127a700ecc2694e4503f107951e80ad49a5d37a
Mampuru <mampuruntwampe@gmail.com>
2023-12-07 23:53:39 +0200

    Updated with fetchflascards function

--- lib/controllers/flash_card_controller.dart
+++ lib/controllers/flash_card_controller.dart
@@ -1,10 +1,20 @@
 class FlashcardController extends GetxController {
   var flashcards = <Flashcard>[].obs;
 
+  @override
+  void onInit() {
+    fetchFlashcardsFromHive(); // Fetch flashcards when the controller initializes
+    super.onInit();
+  }
+
   void addFlashcard(Flashcard card) {
     flashcards.add(card);
   }
 
-// Implement other methods for managing flashcards
+  Future<void> fetchFlashcardsFromHive() async {
+    final box = await Hive.openBox<Flashcard>('flashcardsBox');
+    flashcards.assignAll(box.values.toList());
+  }
+
 }
 
