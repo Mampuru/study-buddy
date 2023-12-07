18d63d6d3b83500e8e4d0f9f169dea29c85fd3cc
Mampuru <mampuruntwampe@gmail.com>
2023-12-07 23:46:52 +0200

    Addition of flash card model

---
+++ lib/models/flash_card_model.dart
@@ -0,0 +1,7 @@
+class Flashcard {
+  final String question;
+  final String answer;
+  final String category;
+
+  Flashcard( {required this.category,required this.question, required this.answer});
+}
