c97bb11472846bd1ec4e5c975c7af2a965b26a93
Mampuru <mampuruntwampe@gmail.com>
2023-12-08 23:42:40 +0200

    Updated main with flash card view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:/flash_card_view.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Container(),
+      home: const FlashcardView(),
     );
   }
 }
