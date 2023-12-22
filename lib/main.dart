6ec3c9f208b56f410ded6e0d7653b3a82531cbeb
Mampuru <mampuruntwampe@gmail.com>
2023-12-22 22:44:18 +0200

    Fixed the red screen missing flashcardController

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:flash_card/controllers/flash_card_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'flash_card_view.dart';
 import 'hive_adapters/flash_card_adapter.dart';
 
 void main() async {
   await Hive.initFlutter();
+
+  Get.put(FlashcardController());
+
   runApp(const MyApp());
 }
 
