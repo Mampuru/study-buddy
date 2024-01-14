a61e9e366260f786cd085b0b7d678c8ec53ebd2f
Mampuru <mampuruntwampe@gmail.com>
2024-01-14 23:57:28 +0200

    Updated the splash icon size

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class MyApp extends StatelessWidget {
           splash: logoPath,
           nextScreen: FlashcardView(),
           splashTransition: SplashTransition.slideTransition,
+          splashIconSize: 250,
           ));
   }
 }
